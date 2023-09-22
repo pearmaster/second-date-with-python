3a8fd02f36f6b198ae78dbb3dfc0140bdea05937
Jacob Brunson <github@jacobbrunson.com>
2023-09-22 14:50:59 +0000

    add a little bit of extra code

--- intro_code.py
+++ intro_code.py
@@ -1,8 +1,12 @@
 lookup = {1: "one", 2: "two"}
 
-def find(n: bool):
-    print("Looking up " + str(n))
+def add(n, word):
+    print("Adding "+ word+ "to lookup as "+str(n))
     global lookup
+    lookup[n] = word
+
+def find(n):
+    print("Looking up " + str(n))
     return lookup[n]
 
 print(find(2))
